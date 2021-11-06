31ec15af9a81953a24cdd127aecab407f295c303
moodtodie <92921817+moodtodie@users.noreply.github.com>
2021-11-06 14:27:41 +0300

    Add files via upload

---
+++ Lab6_7/Lab6_7.c
@@ -0,0 +1,25 @@
+﻿// Lab6_7.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <conio.h>
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	const int n = 50;
+	char str[n];
+
+	printf(" Введите строку состоящию из произвольных символов:\n");
+	fgets(str, n, stdin);			//	Заполнение массива
+
+	printf(" Число из строки:\n");
+
+	for (int i = 0; i < n ; i++)	//	Фильтрация
+		if (isdigit(str[i]))
+			printf(" %c", str[i]);//	Вывод массива
+
+	_getch();
+	return 0;
+}
